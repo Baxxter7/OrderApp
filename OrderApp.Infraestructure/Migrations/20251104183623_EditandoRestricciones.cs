bdc2b7bfe2af8ec68dd7a413d2625ca3fc8e2749
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-11-04 15:11:09 -0600

    Feat: Agregando migraciones

---
+++ OrderApp.Infraestructure/Migrations/20251104183623_EditandoRestricciones.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrderApp.Infraestructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditandoRestricciones : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CustomerName",
+                table: "Orders",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CustomerName",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)",
+                oldMaxLength: 150);
+        }
+    }
+}
