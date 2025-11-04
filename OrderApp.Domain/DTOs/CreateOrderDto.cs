1c86a065cda22dfb2f0e92fe5d7e58c6eea23aca
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-11-04 15:03:54 -0600

    fix: Editando nombres en CreateOrderDto

--- OrderApp.Domain/DTOs/CreateOrderDto.cs
+++ OrderApp.Domain/DTOs/CreateOrderDto.cs
@@ -2,7 +2,7 @@ namespace OrderApp.Domain.DTOs;
 
 public class CreateOrderDto
 {
-    public string CostumerName { get; set; } = string.Empty;
-    public string CostumerEmail { get; set; } = string.Empty;
+    public string CustomerName { get; set; } = string.Empty;
+    public string CustomerEmail { get; set; } = string.Empty;
     public List<CreateOrderItemDto> Items { get; set; } = new List<CreateOrderItemDto>();
 }
