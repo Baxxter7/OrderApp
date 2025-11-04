14ac1078fb8a4958d93964099e761d2a069bb447
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-11-04 15:10:28 -0600

    Feat: Agregando servicios al program

--- OrderApp.Api/Program.cs
+++ OrderApp.Api/Program.cs
@@ -1,17 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using OrderApp.Business.Mapping;
+using OrderApp.Business.Services;
+using OrderApp.Domain.Interfaces;
+using OrderApp.Infraestructure.Data;
+using OrderApp.Infraestructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+//Database
+builder.Services.AddDbContext<AppDbContext>(
+    options => 
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+    );
+
+//Mapper
+builder.Services.AddAutoMapper(cfg => {}, typeof(OrdenMappingProfile));
+
+//Dependency Injection
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IOrderService, OrderService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
